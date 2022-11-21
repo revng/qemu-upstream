bd309245459618a38a3c241c0a1866ea5edf96b9
Alessandro Di Federico <ale@rev.ng>
2022-11-21 15:37:23 +0100

    Do not include target-specific helper.h in target-independent code

--- include/exec/helper-proto.h
+++ include/exec/helper-proto.h
@@ -37,7 +37,9 @@ dh_ctype(ret) HELPER(name) (dh_ctype(t1), dh_ctype(t2), dh_ctype(t3), \
 
 #define IN_HELPER_PROTO
 
+#ifdef TARGET_SPECIFIC
 #include "helper.h"
+#endif // TARGET_SPECIFIC
 #include "accel/tcg/tcg-runtime.h"
 #include "accel/tcg/plugin-helpers.h"
 
