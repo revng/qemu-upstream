a7327f3d98f11b5e8daedfda94839e5844dba441
lxx <1733205434@qq.com>
2024-10-08 11:17:43 +0800

    target/riscv: Add Zilsd and Zclsd extension support
    This patch adds support for the Zilsd and Zclsd extension,
    which is documented at https://github.com/riscv/riscv-zilsd/releases/tag/v0.10
    
    Co-developed-by: SUN Dongya <sundongya@nucleisys.com>
    Co-developed-by: LIU Xu <liuxu@nucleisys.com>
    Co-developed-by: ZHAO Fujin <zhaofujin@nucleisys.com>

--- target/riscv/tcg/tcg-cpu.c
+++ target/riscv/tcg/tcg-cpu.c
@@ -652,6 +652,26 @@ void riscv_cpu_validate_set_extensions(RISCVCPU *cpu, Error **errp)
         return;
     }
 
+    if (cpu->cfg.ext_zilsd && riscv_has_ext(env, RVC)) {
+        cpu_cfg_ext_auto_update(cpu, CPU_CFG_OFFSET(ext_zclsd), true);
+    }
+
+    if (cpu->cfg.ext_zclsd) {
+        cpu_cfg_ext_auto_update(cpu, CPU_CFG_OFFSET(ext_zca), true);
+        cpu_cfg_ext_auto_update(cpu, CPU_CFG_OFFSET(ext_zilsd), true);
+
+        if (riscv_has_ext(env, RVC) && riscv_has_ext(env, RVF)) {
+            error_setg(errp,
+                    "Zclsd cannot be supported together with C and F extension");
+            return;
+        }
+        if (cpu->cfg.ext_zcf) {
+            error_setg(errp,
+                    "Zclsd cannot be supported together with Zcf extension");
+            return;
+        }
+    }
+
     /*
      * Disable isa extensions based on priv spec after we
      * validated and set everything we need.
--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -1162,6 +1162,7 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
 #include "insn_trans/trans_rvzce.c.inc"
 #include "insn_trans/trans_rvzcmop.c.inc"
 #include "insn_trans/trans_rvzicfiss.c.inc"
+#include "insn_trans/trans_zilsd.c.inc"
 
 /* Include decoders for factored-out extensions */
 #include "decode-XVentanaCondOps.c.inc"
