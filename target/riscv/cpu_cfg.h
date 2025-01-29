47f58c68ba7205ed134ebbfca7730ae5181afbab
Anton Johansson <anjo@rev.ng>
2025-01-29 15:59:57 +0100

    target/riscv: Add xqci extension and call decoders
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target/riscv/cpu_cfg.h
+++ target/riscv/cpu_cfg.h
@@ -177,6 +177,7 @@ struct RISCVCPUConfig {
     bool ext_xtheadmempair;
     bool ext_xtheadsync;
     bool ext_XVentanaCondOps;
+    bool ext_xqci;
 
     uint32_t pmu_mask;
     uint16_t vlenb;
@@ -233,5 +234,6 @@ MATERIALISE_EXT_PREDICATE(xtheadmemidx)
 MATERIALISE_EXT_PREDICATE(xtheadmempair)
 MATERIALISE_EXT_PREDICATE(xtheadsync)
 MATERIALISE_EXT_PREDICATE(XVentanaCondOps)
+MATERIALISE_EXT_PREDICATE(xqci)
 
 #endif
