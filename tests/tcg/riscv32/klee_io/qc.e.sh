f100b0fe49f62966634d9aaaac8ff87d719bdda7
Anton Johansson <anjo@rev.ng>
2025-03-03 15:51:32 +0100

    [TMP] tests/tcg: Add Xqci instruction tests
    Hardcodes makefile rules for building/running Xqci instruction tests.
    This should be moved to assembly compiled from a docker container once
    we have access to a toolchain.
    
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- tests/tcg/riscv32/klee_io/qc.e.sh
+++ tests/tcg/riscv32/klee_io/qc.e.sh
@@ -1,1 +1,18 @@
-0 0 0 
+- variables:
+  - name: "imm"
+    in: 67108863
+  - name: "rs2"
+    in: 0
+  - name: "rs1"
+    in: 4227858944
+  overflow: 1
+  underflow: 0
+- variables:
+  - name: "imm"
+    in: 0
+  - name: "rs2"
+    in: 0
+  - name: "rs1"
+    in: 0
+  overflow: 0
+  underflow: 0
