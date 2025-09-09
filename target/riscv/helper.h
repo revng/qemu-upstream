f5951f8c831b3b62848061a5b66121408bdb21a6
Anton Johansson <anjo@rev.ng>
2025-09-09 23:17:51 +0200

    target/riscv: helper.h update include names
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -1283,4 +1283,4 @@ DEF_HELPER_5(vsm4k_vi, void, ptr, ptr, i32, env, i32)
 DEF_HELPER_4(vsm4r_vv, void, ptr, ptr, env, i32)
 DEF_HELPER_4(vsm4r_vs, void, ptr, ptr, env, i32)
 
-#include "xqci/xqci_helper.h"
+#include "xqci/xqci-helper.h"
