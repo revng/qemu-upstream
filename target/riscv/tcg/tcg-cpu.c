164fe75208ef650f03f8275093671a65e83432a0
Anton Johansson <anjo@rev.ng>
2025-09-09 23:23:18 +0200

    target/riscv: tcg-cpu.c verify Xqci/Xqccmp extension requirements
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target/riscv/tcg/tcg-cpu.c
+++ target/riscv/tcg/tcg-cpu.c
@@ -672,6 +672,29 @@ void riscv_cpu_validate_set_extensions(RISCVCPU *cpu, Error **errp)
         }
     }
 
+    /* Verify conflicts and requirements for Xqci/Xqccmp extensions */
+
+    if (cpu->cfg.ext_xqccmp) {
+        if ((riscv_has_ext(env, RVC) && riscv_has_ext(env, RVD)) || cpu->cfg.ext_zcd || cpu->cfg.ext_zcmp) {
+            error_setg(errp, "Any of the extensions: xqccmp conflicts with C,D,Zcd,Zcmp");
+            return;
+        }
+    }
+
+    if (cpu->cfg.ext_xqcicm || cpu->cfg.ext_xqciac) {
+        if ((riscv_has_ext(env, RVC) && riscv_has_ext(env, RVD)) || cpu->cfg.ext_zcd) {
+            error_setg(errp, "Any of the extensions: xqcicm,xqciac conflicts with C,D,Zcd");
+            return;
+        }
+    }
+
+    if (cpu->cfg.ext_xqciac || cpu->cfg.ext_xqcilia || cpu->cfg.ext_xqcibi || cpu->cfg.ext_xqcisim || cpu->cfg.ext_xqccmp || cpu->cfg.ext_xqcibm || cpu->cfg.ext_xqciint || cpu->cfg.ext_xqcicm || cpu->cfg.ext_xqcilb || cpu->cfg.ext_xqcili || cpu->cfg.ext_xqcisync) {
+        if (!cpu->cfg.ext_zca) {
+            error_setg(errp, "Any of the extensions: xqciac,xqcilia,xqcibi,xqcisim,xqccmp,xqcibm,xqciint,xqcicm,xqcilb,xqcili,xqcisync requires Zca");
+            return;
+        }
+    }
+
     /*
      * Disable isa extensions based on priv spec after we
      * validated and set everything we need.
